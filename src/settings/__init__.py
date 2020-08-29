a2931766b9f1906a87373226a06f6629b1fb9ebb
vadolasi <vitor036daniel@gmail>
2020-08-29 17:37:45 -0300

    [internal] Upgrade settings manangement

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -3,6 +3,9 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+from src.settings.shared import *
+
+
 if env("SETTINGS") == "development":
     from src.settings.development import *
 else:
--- src/settings/production.py
+++ src/settings/production.py
@@ -9,146 +9,22 @@
 * For the full list of settings and their values, see
 ? https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from pathlib import Path
+from src.settings.common import *
 
 import environ
-
-# * Build paths inside the project like this: BASE_DIR / "subdir".
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
-
-# * Django environ
-# ? https://django-environ.readthedocs.io/en/latest/
 
 env = environ.Env()
 environ.Env.read_env()
 
-
-# * Quick-start development settings - unsuitable for production
-# ? See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-#! SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-#! SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ["https://adpaz.pythonanywhere.com"]
+DATABASES = {"default": env.db("DATABASE_URL")}
 
-
-# * Application definition
-
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "graphene_django",
-    "corsheaders",
-    "reversion",
-]
-
-MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
-
-ROOT_URLCONF = "src.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = "src.wsgi.application"
-
-
-# * Database
-# ? https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DATABASE"),
-        "USER": env("DATABASE_PORT"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": env("DATABASE_HOST"),
-        "PORT": env("DATABASE_PORT"),
-    }
-}
-
-
-# * Password validation
-# ? https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
-# * Password hashes
-# ? https://docs.djangoproject.com/en/3.1/topics/auth/passwords/#using-argon2-with-django
-
-PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-]
-
-
-# * Internationalization
-# ? https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = "pt-br"
-
-TIME_ZONE = "America/Sao_Paulo"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# * Static files (CSS, JavaScript, Images)
-# ? https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = "/static/"
-STATIC_ROOT = "static"
-
-CORS_ORIGIN_ALLOW_ALL = True
+TENANT_USERS_DOMAIN = env("DOMAIN")
